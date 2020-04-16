031ea89bba622884991c0a59a2eb392d9ab285ed
Olav Vahtras <vahtras@kth.se>
2020-04-16 11:27:31 +0200

    fourth

--- dftfunc.py
+++ dftfunc.py
@@ -131,3 +131,25 @@ static void
 """
 
         return code
+
+    def fourth(self):
+        code = f"""
+static void
+{self.name}_fourth(FunFourthFuncDrv *ds, real factor, const FunDensProp* dp)
+{{
+  if (dp->rhoa>{self.name.upper()}_THRESHOLD) {{
+     ds->df1000 += {sympy.ccode(self.Fa.diff(self.ra))}*factor;
+     ds->df2000 += {sympy.ccode(self.Fa.diff(self.ra, self.ra))}*factor;
+     ds->df3000 += {sympy.ccode(self.Fa.diff(self.ra, self.ra, self.ra))}*factor;
+     ds->df4000 += {sympy.ccode(self.Fa.diff(self.ra, self.ra, self.ra, self.ra))}*factor;
+     }}
+  if (dp->rhob>{self.name.upper()}_THRESHOLD) {{
+     ds->df0100 += {sympy.ccode(self.Fb.diff(self.rb))}*factor;
+     ds->df0200 += {sympy.ccode(self.Fb.diff(self.rb, self.rb))}*factor;
+     ds->df0300 += {sympy.ccode(self.Fb.diff(self.rb, self.rb, self.rb))}*factor;
+     ds->df0400 += {sympy.ccode(self.Fb.diff(self.rb, self.rb, self.rb, self.rb))}*factor;
+     }}
+}}
+"""
+
+        return code
